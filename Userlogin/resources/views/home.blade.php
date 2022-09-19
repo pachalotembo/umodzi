0a717f4fff9bd35eab0d05aa8af5b0029ad71a91
pachalotembo <pacharotembo@gmail.com>
2022-09-19 11:46:38 +0200

    laravel doc

---
+++ Userlogin/resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+  <div class="flex-shrink-0 p-3 bg-white" style="width: 20%;">
+    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-5 fw-semibold">Welcome</span>
+    </a>
+    <ul class="list-unstyled ps-0">
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+          Home
+        </button>
+        <div class="collapse show" id="home-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Overview</a></li>
+            <li><a href="#" class="link-dark rounded">Updates</a></li>
+            <li><a href="#" class="link-dark rounded">Reports</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+          View Tables
+        </button>
+        <div class="collapse" id="dashboard-collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+            <li><a href="#" class="link-dark rounded">Overview</a></li>
+            <li><a href="#" class="link-dark rounded">Male</a></li>
+            <li><a href="#" class="link-dark rounded">Female</a></li>
+            
+          </ul>
+        </div>
+      </li>
+      
+      <li class="border-top my-3"></li>
+      
+    </ul>
+  </div>
+
+@endsection
